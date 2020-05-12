b5bd7993e046e9ca0b8d12a5f88d4e18fd3f31b1
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-12 05:16:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,9 @@
 class Polygon {
   constructor(sides) {
     this.sides = sides;
-    this.count = this.sides.length;
   }
   
   get countSides() {
-    return this.
+    return this.sides.length;
   }
 }
